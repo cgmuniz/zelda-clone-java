82fc90c891ef4ce81d5941e1ea5017768f337aae
cgmuniz <cauamuniz00@gmail.com>
2024-04-02 01:11:47 -0300

    personagem anda

---
+++ src/com/cgmuniz/entities/Player.java
@@ -0,0 +1,25 @@
+package com.cgmuniz.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Player extends Entity {
+	
+	public boolean right, left, up, down;
+	public double speed = 1;
+
+	public Player(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+	}
+	
+	public void tick() {
+		if(right)
+			x+=speed;
+		else if(left)
+			x-=speed;
+		if(up)
+			y-=speed;
+		else if (down)
+			y+=speed;
+	}
+
+}
