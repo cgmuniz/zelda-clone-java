090bce8e6aa9126f91a028327c5defb78b8a086c
cgmuniz <cauamuniz00@gmail.com>
2024-04-04 00:06:47 -0300

    inimigos andam, animação, colisão e dano

--- src/com/cgmuniz/main/Game.java
+++ src/com/cgmuniz/main/Game.java
@@ -10,9 +10,11 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
+import com.cgmuniz.entities.Enemy;
 import com.cgmuniz.entities.Entity;
 import com.cgmuniz.entities.Player;
 import com.cgmuniz.graficos.Spritesheet;
@@ -31,12 +33,18 @@ public class Game extends Canvas implements Runnable, KeyListener{
 	private BufferedImage image;
 	
 	public static List<Entity> entities;
+	public static List<Enemy> enemies;
+	
 	public static Spritesheet spritesheet;
 	public static Player player;
 	
 	public static World world;
 	
+	public static Random rand;
+	
 	public Game() {
+		rand = new Random();
+		
 		addKeyListener(this);
 		
 		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -45,6 +53,7 @@ public class Game extends Canvas implements Runnable, KeyListener{
 		// Inicializando objetos
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 		entities = new ArrayList<Entity>();
+		enemies = new ArrayList<Enemy>();
 		spritesheet = new Spritesheet("/spritesheet.png");
 		
 		player = new Player(0, 0, 16, 16, spritesheet.getSprite(32, 0, 16, 16));
@@ -122,6 +131,8 @@ public class Game extends Canvas implements Runnable, KeyListener{
 		double delta = 0;
 		int frames = 0;	
 		double timer = System.currentTimeMillis();
+		
+		requestFocus();
 		
 		while(isRunning) {
 			long now = System.nanoTime();
